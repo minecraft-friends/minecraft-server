9ab523ecbebfa9c0f2b9793e3c76ad95f85bded9
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2022-02-06 23:53:57 -0600

    update TODO with link to reference

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 ./aws/install
 
-# TODO: need to setup a role to access EC2 metadata to get credentials (and configure an IAM user for the service)
+# TODO: need to setup a role to access EC2 metadata to get credentials (and configure an IAM user for the service): https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-metadata.html
 # shellcheck disable=SC2154
 # shellcheck disable=SC2086
 aws s3 cp s3://${bucket.bucket}/${snapshot.name}.${snapshot.ext} .
