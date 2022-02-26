20b67a45a6965be04b29bac80a3db62d4cbbd3f7
Gonzo Guajardo <gonzo.guajardo@gmail.com>
2022-02-26 13:41:39 -0600

    Add private subnet, add nat gateway for internet access in private subnet

--- minecraft_discord_bot.tf
+++ minecraft_discord_bot.tf
@@ -2,7 +2,7 @@ module "minecraft_discord_bot" {
   source = "./modules/minecraft-discord-bot"
   config_bucket_key_name = "config.json"
   config_bucket_name = "minecraft-discord-bot"
-  ecs_subnet_id = aws_subnet.minecraft_server_public_subnet.id
+  ecs_subnet_id = aws_subnet.private_subnet.id
   ecs_task_image = "622452799301.dkr.ecr.us-west-2.amazonaws.com/minecraft-discord-bot:latest"
   env = local.environment
   tags = local.tags
